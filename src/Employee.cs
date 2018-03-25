f52f4d6cb2e5074c2ae2010e00600332537acbbb
Zhengquan Yang <yangzhengquan@gmail.com>
2018-03-26 07:25:41 +0800

    ready to remove

--- src/Employee.cs
+++ src/Employee.cs
@@ -3,25 +3,27 @@ using System;
 namespace refactoring {
     public class Engineer : Employee {
         public Engineer () { }
-        public override int EmployeeType => Employee.ENGINEER;
+        public override int GetPayroll () {
+            return MonthlySalary;
+        }
     }
     public class Salesman : Employee {
         public Salesman () { }
-        public override int EmployeeType => Employee.SALESMAN;
+        public override int GetPayroll () {
+            return MonthlySalary + Commission;
+        }
     }
     public class Manager : Employee {
         public Manager () { }
-        public override int EmployeeType => Employee.MANAGER;
+        public override int GetPayroll () {
+            return MonthlySalary + Commission + Bonus;
+        }
     }
 
     public class Employee {
-        protected Employee () {
-            this.EmployeeType = EmployeeType;
-        }
+        protected Employee () { }
 
-        private Employee (int employeeType) {
-            EmployeeType = employeeType;
-        }
+        private Employee (int employeeType) { }
 
         public static Employee Create (int employeeType) {
             switch (employeeType) {
@@ -32,11 +34,10 @@ namespace refactoring {
                 case Employee.MANAGER:
                     return new Manager ();
                 default:
-                    return new Employee (employeeType);
+                    return new Employee ();
             }
         }
 
-        public virtual int EmployeeType { get; private set; }
         public const int ENGINEER = 0;
         public const int SALESMAN = 1;
         public const int MANAGER = 2;
@@ -45,17 +46,8 @@ namespace refactoring {
         public int Commission { get; set; }
         public int Bonus { get; set; }
 
-        public int GetPayroll () {
-            switch (EmployeeType) {
-                case ENGINEER:
-                    return MonthlySalary;
-                case SALESMAN:
-                    return MonthlySalary + Commission;
-                case MANAGER:
-                    return MonthlySalary + Commission + Bonus;
-                default:
-                    throw new Exception ("Incorrect Employee Code");
-            }
+        public virtual int GetPayroll () {
+            throw new Exception ("Incorrect Employee Code");
         }
     }
 }
